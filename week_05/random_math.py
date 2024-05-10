8c2ef933952677531df1814e7ac86cb0821d130d
Surajit Bose <verbose@duck.com>
2024-05-10 16:32:46 -0700

    Better document random number programs

--- week_05/random_math.py
+++ week_05/random_math.py
@@ -5,7 +5,7 @@ Illustrate use of import statements, the math library, and the random module.
 Along with companion module math_random.py, illustrate the difference between
 from ... import and a bare import. 
 
-Programmer: Surajit A. Bose
+Programmer: Surajit A. Bose, Date: 2024.05.10
 """
 
 # import statements
@@ -17,6 +17,11 @@ def main():
     """
     Generate four random numbers between 200 and 1000. Calculate their square roots.
     Print each number and its square root to the screen. 
+    
+    Preconditions: None
+    
+    Postconditions: Four random numbers in the specified range 
+    are printed to screen along with their square roots.
     """
     for i in range(4):
         my_num = randint(200, 1000)
